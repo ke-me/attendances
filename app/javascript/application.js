1af3c6cd769bdd9245d9920dca045b4fd36e47bc
ke-me <fukudakei@fukudatkonpyuta.lan>
2024-10-20 17:25:04 +0900

    javascript修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,21 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+
+//= require jquery3
+//= require jquery_ujs
+//= require popper
+//= require bootstrap
+
+// turboが効かないのでコメントアウト
+// import "@hotwired/turbo-rails"
+
+function switchDisplay() {
+    document.getElementById('flash-message').classList.add('none');
+  }
+
+if (document.getElementById('flash-message')) {
+  setTimeout(() => {
+    switchDisplay();
+  }, 3000);
+}
+
+console.log('test');
